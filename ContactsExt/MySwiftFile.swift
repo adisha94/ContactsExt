3f2a28053db2fddbed30dbec3332cf501994c152
ADISH BETAWAR <abetawar@gmail.com>
2015-12-29 12:21:29 -0800

    new info

--- ContactsExt/MySwiftFile.swift
+++ ContactsExt/MySwiftFile.swift
@@ -1,10 +1,23 @@
-//
-//  MySwiftFile.swift
-//  ContactsExt
-//
-//  Created by Adish Betawar on 12/29/15.
-//  Copyright © 2015 Adish Betawar. All rights reserved.
-//
-
 import Foundation
 
+struct Contact
+{
+    let name: String
+    let phone_Number: Int
+    let school: String
+    let id: Int // randomely assign if possible with number generators
+    let is_working : Bool
+    
+    func get_Name() -> String
+    {
+        return name;
+    }
+    func get_Phone_Number() -> Int
+    {
+        return phone_Number; // 555-555-5555 style number format
+    }
+    func get_School() -> String
+    {
+        return school;
+    }
+}
