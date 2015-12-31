71b282bffaeacad79ebff4f3b36ca6ceb1f54a65
ADISH BETAWAR <abetawar@gmail.com>
2015-12-30 22:16:43 -0800

    changed

---
+++ ContactsExt/ContactTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ContactTableViewController.swift
+//  ContactsExt
+//
+//  Created by Adish Betawar on 12/30/15.
+//  Copyright © 2015 Adish Betawar. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewController: UITableViewController
+{
+    @IBOutlet weak var contact_Entry: UIView!
+
+}
